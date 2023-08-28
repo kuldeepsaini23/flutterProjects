2b40cbd7d9eeccf4f4cc9db722f1d84f4ec70cfc
Kuldeep Saini <89005887+kuldeepsaini23@users.noreply.github.com>
2023-08-28 18:51:04 +0530

    Add files via upload

---
+++ quiz_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Software\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Udmey Courses\\Flutter\\Section 3 QUiz App\\quiz_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Software\\flutter"
+  "PROJECT_DIR=D:\\Udmey Courses\\Flutter\\Section 3 QUiz App\\quiz_app"
+  "FLUTTER_ROOT=D:\\Software\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Udmey Courses\\Flutter\\Section 3 QUiz App\\quiz_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Udmey Courses\\Flutter\\Section 3 QUiz App\\quiz_app"
+  "FLUTTER_TARGET=D:\\Udmey Courses\\Flutter\\Section 3 QUiz App\\quiz_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Udmey Courses\\Flutter\\Section 3 QUiz App\\quiz_app\\.dart_tool\\package_config.json"
+)
